4b1b5d1c4e4b043fc7cfa6e348e896c54d50af16
Shane D <dieckma3@unlv.nevada.edu>
2012-03-22 09:49:19 -0700

    WTF: can't get controller to work with post data
    whenever the form is submitted, no mapped functions are called - why?

--- offbudget/src/main/java/edu/unlv/kilo/web/TaggingController.java
+++ offbudget/src/main/java/edu/unlv/kilo/web/TaggingController.java
@@ -1,5 +1,6 @@
 package edu.unlv.kilo.web;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,17 +21,39 @@ import edu.unlv.kilo.domain.TransactionEntity;
 @RequestMapping("/tagging/**")
 @Controller
 public class TaggingController {
+	int test;
 
+	// WTF: This never gets called when post data is submitted
 	@RequestMapping(method = RequestMethod.POST, value = "{id}")
 	public void post(@PathVariable Long id, ModelMap modelMap,
 			HttpServletRequest request, HttpServletResponse response) {
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
-	public void get(ModelMap modelMap) {
+	
+
+	// WTF: This never gets called when post data is submitted
+	@RequestMapping(method = RequestMethod.POST)
+	public void pasta(ModelMap modelMap, HttpServletRequest request) {
 //		modelMap.addAttribute("msg","</h4><h3>helloworld</h3><h4>");
 		TaggingView taggingView = new TaggingView();
 		taggingView.render(modelMap);
+
+		test = 2;
+		modelMap.addAttribute("test", test);
+		
+	}
+
+	// WTF: This never gets called when post data is submitted
+	@RequestMapping(method = RequestMethod.GET)
+	public String get(ModelMap modelMap, HttpServletRequest request) {
+//		modelMap.addAttribute("msg","</h4><h3>helloworld</h3><h4>");
+		TaggingView taggingView = new TaggingView();
+		taggingView.render(modelMap);
+
+		test = 1;
+		modelMap.addAttribute("test", test);
+		
+		return "tagging/index"; // this return does nothing
 	}
 
 	@RequestMapping
--- offbudget/src/main/java/edu/unlv/kilo/web/TaggingView.java
+++ offbudget/src/main/java/edu/unlv/kilo/web/TaggingView.java
@@ -65,34 +65,24 @@ public class TaggingView {
 	}
 	
 	/**
+	 * Internal: Creates a form for deleting a transaction
+	 * @param id
+	 * @return
+	 */
+	private String renderDeleteForm(int id) {
+		String out = "";
+		out += "<form id=\"command\" action=\"/offbudget/tagging/index\" method=\"post\"><input type=\"hidden\" name=\"_method\" value=\"DELETE\"/>";
+		out += "<input type=\"hidden\" name=\"_id\" value=\"" + id + "\"/>";
+		out += "<input onclick=\"return confirm('Are you sure want to delete this transaction?');\" value=\"Delete Transaction\" type=\"image\" title=\"Delete Transaction\" src=\"/offbudget/resources/images/delete.png\" class=\"image\" alt=\"Delete Transaction\"/>";
+		out += "</form>";
+		return out;
+	}
+	
+	/**
 	 * Generates HTML string for rendering by client
 	 * @param modelMap
 	 */
 	void render(ModelMap modelMap) {
-		List<TransactionEntity> transactions = new LinkedList<TransactionEntity>();
-		
-		{
-			TransactionEntity a = new TransactionEntity();
-			a.setAmount(new MoneyValue(300));
-			a.setTimeof(new Date(2012,03,01));
-			TransactionDescription desc = new TransactionDescription();
-			desc.setDescription("Test transaction A");
-			a.setDescription(desc);
-			
-			transactions.add(a);
-		}
-		
-		{
-			TransactionEntity a = new TransactionEntity();
-			a.setAmount(new MoneyValue(300));
-			a.setTimeof(new Date(2012,03,05));
-			TransactionDescription desc = new TransactionDescription();
-			desc.setDescription("Test transaction B");
-			a.setDescription(desc);
-			
-			transactions.add(a);
-		}
-		
 		modelMap.addAttribute("transactions", transactions);
 		
 		String output = "<table>" +
@@ -100,12 +90,17 @@ public class TaggingView {
 				"		<th>Date</th>" +
 				"		<th>Description</th>" +
 				"		<th>Amount</th>" +
+				"		<th />" +
 				"	</tr>" +
 				"";
 		
+		int id = 0;
 		for (TransactionEntity transactionEntity : transactions) {
+			++id;
+			
 			output += "<tr>";
 			output += transactionEntity.getTDset();
+			output += "<td>" + renderDeleteForm(id) + "</td>";
 			output += "</tr>";
 		}
 		
