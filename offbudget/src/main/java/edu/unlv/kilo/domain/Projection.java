6cc1676afc464dd2ffe5e4f5fc9aa5e9c719dfcc
Dane Strong <chrono48@gmail.com>
2012-03-24 17:09:47 -0700

    Couple modifications.
    Signed-off-by: Dane Strong <chrono48@gmail.com>

--- offbudget/src/main/java/edu/unlv/kilo/domain/Projection.java
+++ offbudget/src/main/java/edu/unlv/kilo/domain/Projection.java
@@ -26,16 +26,19 @@ public class Projection {
      * 
      * @param startDate The date to begin the projection.
      * @param endDate The date to finish the projection.
-     * @param interval The interval with which to calculate the data points (i.e. weekly, monthly, etc).
+     * @param interval The interval in # of days with which to calculate the data points.
      * @return The requested data for plotting the graphs in a List.
      */
-    public List<MoneyValue> getData(Date startDate, Date endDate, int interval){
+    public List<MoneyValue> getData(Date startDate, Date endDate, long interval){
     	MoneyValue current;
-    	ItemEntity item = new ItemEntity();				// placeholder
-    	ItemAdjustment adjust = new ItemAdjustment();	// placeholder
+    	ItemEntity item = new ItemEntity();						// placeholder
+    	ItemAdjustment adjust = new ItemAdjustment();			// placeholder
     	Set<TransactionEntity> trans = item.getTransactions();
     	Iterator<TransactionEntity> it = trans.iterator();
     	
+    	long period = endDate.getTime() - startDate.getTime();
+    	
+    	// The while loop iterates through the Transactions in the Item.
     	while (it.hasNext()){
     		current = it.next().getAmount();
     		
